bb1202f752485d2dd85b40ac6790e62cbbd3ed83
unknown <iRepair203@gmail.com>
2022-10-26 20:46:43 +0300

    added array product list

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const productContainer = document.getElementById("product-container");
+
+const productsList = ["Black Tea", "Green tea"]
+
+
 const blackTea = document.createElement("div");
 const greenTea = document.createElement("div");
 
-blackTea.innerHTML = "Black Tea";
-greenTea.innerHTML = "Green Tea";
+blackTea.innerHTML = productsList[0];
+greenTea.innerHTML = productsList[1];
 
 blackTea.classList.add("product")
 greenTea.classList.add("product")
